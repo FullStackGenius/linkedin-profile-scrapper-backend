ff5a693b5eee5041b556b6e0d3f64674784ce75d
FullStackGenius <upliftwebdev@gmail.com>
2025-08-04 13:05:29 +0530

    change db password env

--- config/config.js
+++ config/config.js
@@ -4,14 +4,14 @@ require('dotenv').config();
 module.exports = {
   development: {
     username: process.env.DB_USER || 'root',
-    password: process.env.DB_PASS || null,
+    password: process.env.DB_PASSWORD || null,
     database: process.env.DB_NAME || 'linkedin_scraper',
     host: process.env.DB_HOST || '127.0.0.1',
     dialect: 'mysql'
   },
   production: {
     username: process.env.DB_USER,
-    password: process.env.DB_PASS,
+    password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     dialect: 'mysql'
